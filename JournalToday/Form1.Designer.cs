56aa60bd8cbc077440717de7d47431e5c68481dd
aeshirey <adam.shirey@gmail.com>
2015-02-16 21:23:01 -0800

    Initial commit

---
+++ JournalToday/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace JournalToday
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbJournalText = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabelCharacterCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbJournalText
+            // 
+            this.tbJournalText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbJournalText.Location = new System.Drawing.Point(12, 58);
+            this.tbJournalText.Multiline = true;
+            this.tbJournalText.Name = "tbJournalText";
+            this.tbJournalText.Size = new System.Drawing.Size(688, 535);
+            this.tbJournalText.TabIndex = 1;
+            this.tbJournalText.TextChanged += new System.EventHandler(this.tbJournalText_TextChanged);
+            this.tbJournalText.Leave += new System.EventHandler(this.tbJournalText_Leave);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(13, 13);
+            this.dateTimePicker1.MinDate = new System.DateTime(2015, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(398, 31);
+            this.dateTimePicker1.TabIndex = 2;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabelCharacterCount,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 591);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(712, 38);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabelCharacterCount
+            // 
+            this.statusLabelCharacterCount.Name = "statusLabelCharacterCount";
+            this.statusLabelCharacterCount.Size = new System.Drawing.Size(142, 33);
+            this.statusLabelCharacterCount.Text = "0 characters";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.IsLink = true;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(214, 33);
+            this.toolStripStatusLabel1.Text = "Journal Today v1.0";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 629);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.tbJournalText);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "formMain";
+            this.Text = "Journal Today";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formMain_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbJournalText;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabelCharacterCount;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ JournalToday/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SQLite;
+
+namespace JournalToday
+{
+    public partial class formMain : Form
+    {
+        private const string JOURNAL_DB = "journalToday.sqlite";
+        private SQLiteConnection dbConnection;
+        
+        private Func<DateTime, UInt32> formatDate = date => Convert.ToUInt32(date.Year * 10000 + date.Month * 100 + date.Day);
+
+        public formMain()
+        {
+            InitializeComponent();
+
+            try
+            {
+                dbConnection = new SQLiteConnection(JOURNAL_DB);
+
+                if (!System.IO.File.Exists(JOURNAL_DB))
+                    dbConnection.CreateTable<JournalEntry>();
+            }
+            catch
+            {
+                MessageBox.Show("Unable to open or create journal database file '" + JOURNAL_DB + "'");
+
+                dateTimePicker1.Enabled = false;
+                tbJournalText.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Load the entry (if one exists) for the selected date
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            var selectedDate = ((DateTimePicker)sender).Value.Date;
+
+            var journalEntry = dbConnection.Table<JournalEntry>().FirstOrDefault(je => je.JournalDate == formatDate(selectedDate));
+            tbJournalText.Text = journalEntry == null ? string.Empty : journalEntry.JournalText;
+
+
+            tbJournalText.Enabled = selectedDate == DateTime.Now.Date;
+        }
+
+        private void tbJournalText_Leave(object sender, EventArgs e)
+        {
+            var text = ((TextBox)sender).Text;
+
+            var journalEntry = new JournalEntry()
+            {
+                JournalDate = formatDate(DateTime.Now),
+                JournalText = tbJournalText.Text
+            };
+            dbConnection.InsertOrReplace(journalEntry);
+        }
+
+        private void formMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            dbConnection.Commit();
+            dbConnection.Close();
+        }
+
+        private void tbJournalText_TextChanged(object sender, EventArgs e)
+        {
+            int charCount = ((TextBox)sender).Text.Count(c => !char.IsWhiteSpace(c));
+            statusLabelCharacterCount.Text = charCount.ToString() + (charCount == 1 ? " character" : " characters");
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://dingostick.com");
+        }
+    }
+}
